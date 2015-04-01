46b420190270e9870b93b8a70bedee4b62a13827
Brandon Turner <bt@brandonturner.net>
2015-04-01 12:47:42 -0500

    Prepare v0.1.5 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 GitHub Notification Proxy CHANGELOG
 ===================================
 
+v0.1.5
+------
+- Add extra client logging
+- Fix incorrect http request bug
+- Update chef cookbook dependencies
+- Update default Ruby to 2.2.1 in cookbook
+
 v0.1.4
 ------
 - Fix bug that caused wrong notification to be delivered
--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "engineeringservices@rapid7.com"
 license          "All rights reserved"
 description      "Installs and configures the GitHub Notification Proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.5"
 
 supports 'ubuntu'
 
