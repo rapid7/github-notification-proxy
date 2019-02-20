bd3afb88238fc5b7a02df60bbfc07f27fb54bf62
Ulrich Dangel <ulrich_dangel@rapid7.com>
2019-02-20 09:05:55 +0000

    Prepare for 0.1.7
    The rvm url was adjusted as berks otherwise was always complaining about
    location change. This just changes it to the underlying git url.
    
    PD-19715

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "engineeringservices@rapid7.com"
 license          "All rights reserved"
 description      "Installs and configures the GitHub Notification Proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.7"
 
 supports 'ubuntu'
 
